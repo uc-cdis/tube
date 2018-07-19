fb93751a3e8309b170b9a247e17193b372853745
Thanh Nguyen <thanhnd@uchicago.edu>
2018-07-19 16:53:08 -0500

    feat(parser): add mapping and parser

--- tube/spark/translator.py
+++ tube/spark/translator.py
@@ -11,6 +11,14 @@ def extract_metadata(str_value):
     return props
 
 
+def extract_link(str_value):
+    strs = ast.literal_eval(str_value)
+    props = {}
+    props['src_id'] = strs[4]
+    props['dst_id'] = strs[5]
+    return props
+
+
 def flatten_files_to_lists(pair):
     f, text = pair
     return [line for line in text.splitlines()]
@@ -26,9 +34,14 @@ class Gen3Translator(object):
         self.sc = sc
         self.hdfs_path = make_sure_hdfs_path_exist(config.HDFS_DIR, sc)
 
-    def translate_a_table(self, table_name):
+    def translate_table(self, table_name):
         df = self.sc.wholeTextFiles(os.path.join(self.hdfs_path, table_name)).flatMap(flatten_files_to_lists)
         df = df.map(extract_metadata)
+        df.saveAsTextFile(os.path.join(self.hdfs_path, 'export', table_name))
+
+    def translate_edge(self, table_name):
+        df = self.sc.wholeTextFiles(os.path.join(self.hdfs_path, table_name)).flatMap(flatten_files_to_lists)
+        df = df.map(extract_link)
         df.saveAsTextFile(os.path.join(self.hdfs_path, 'export', table_name))
 
     def run_etl(self):
@@ -38,4 +51,6 @@ class Gen3Translator(object):
                 line = line.rstrip('\n')
                 print(line)
                 if line.startswith('node_'):
-                    self.translate_a_table(line)
+                    self.translate_table(line)
+                if line.startswith('edge_'):
+                    self.translate_edge(line)
