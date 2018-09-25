db11377d44b6bee3e046c50aba9178bf3937bd6e
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2018-09-24 19:05:21 -0500

    fix/spark: increase Java heap size for spark to 2GB (#33)

--- tube/spark/__init__.py
+++ tube/spark/__init__.py
@@ -7,9 +7,12 @@ def make_spark_context(config):
     Makes a spark and sqlContext
     '''
     os.environ['PYSPARK_SUBMIT_ARGS'] = \
-        '--jars {}/dist/elasticsearch-spark-20_2.11-{}.jar pyspark-shell'\
+        '--jars {}/dist/elasticsearch-spark-20_2.11-{}.jar pyspark-shell' \
             .format(config.ES_HADOOP_HOME_BIN, config.ES_HADOOP_VERSION)
-    conf = SparkConf().setAppName(config.APP_NAME)
+    conf = SparkConf() \
+        .set('spark.executor.memory', '2g') \
+        .set('spark.driver.memory', '2g') \
+        .setAppName(config.APP_NAME)
     if config.RUNNING_MODE == 'Dev':
         # We should only use the value of `config.spark_master` in
         # a test context. Production runs need to set the Spark Master
