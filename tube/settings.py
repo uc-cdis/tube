f1b8bf55fdf26f3ec837825075b637e179cb279a
Thanh Nguyen <thanhnd@uchicago.edu>
2019-02-12 23:29:01 -0600

    fix(env): set env in python code

--- tube/settings.py
+++ tube/settings.py
@@ -57,3 +57,4 @@ APP_NAME = 'Gen3 ETL'
 os.environ['PYSPARK_SUBMIT_ARGS'] = \
     '--jars {}/dist/elasticsearch-spark-20_2.11-{}.jar pyspark-shell' \
         .format(ES_HADOOP_HOME_BIN, ES_HADOOP_VERSION)
+os.environ['HADOOP_CLIENT_OPTS'] = os.getenv('HADOOP_CLIENT_OPTS', '')
