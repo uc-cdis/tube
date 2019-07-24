060af996e07d0997a6581b3a5df99ab82571f0be
Nish <38463583+nishhub@users.noreply.github.com>
2019-07-24 10:27:25 -0500

    Update etl_parser.py

--- etl_parser.py
+++ etl_parser.py
@@ -2,27 +2,35 @@ import yaml
 from pprint import pprint
 from collections import defaultdict
 
-path = '/tmp/ETLMap.yaml'
-# open the yaml file and load it as dictionary
-with open(path) as yamlfile:
-    try:
-        #print(yaml.safe_load(yamlfile))
-        data = dict(yaml.safe_load(yamlfile))
-    except yaml.YAMLError as exc:
-        print(exc)
+class Parser(object):
+    
+    def __init__(self):
+        pass
+        
+    def parser(self):
+        #with open('/tmp/ETLMapping.yaml') as yamlfile:
+            #try:
+                #print(yaml.safe_load(yamlfile))
+                #data = dict(yaml.safe_load(yamlfile))
+            #except yaml.YAMLError as exc:
+                #print(exc)
 
-props_values = []
-root_dict = {}
-flatten_dict = {}
+        props_values = []
+        root_dict = {}
+        flatten_dict = {}
 
-for i in range(len(data['mappings'])):
-        root_dict.update({data['mappings'][i].get("doc_type", ""): ""})
-        root_props = data['mappings'][i].get("props", "")
-        root_dict[list(root_dict)[i]] = [d['name'] for d in root_props]
-        for i in range(len(one_data['flatten_props'])):
-            flatten_dict.update({one_data['flatten_props'][i].get("path", ""): ""})
-            root_props = one_data['flatten_props'][i].get("props", "")
-            flatten_dict[list(flatten_dict)[i]] = [d['name'] for d in root_props]
-
-print(root_dict)
-print(flatten_dict)
+        for i in range(len(data['mappings'])):
+            root_dict.update({data['mappings'][i].get("doc_type", ""): ""})
+            root_props = data['mappings'][i].get("props", "")
+            root_dict[list(root_dict)[i]] = [d['name'] for d in root_props]
+            for i in range(len(one_data['flatten_props'])):
+                flatten_dict.update({one_data['flatten_props'][i].get("path", ""): ""})
+                root_props = one_data['flatten_props'][i].get("props", "")
+                flatten_dict[list(flatten_dict)[i]] = [d['name'] for d in root_props]
+            return flatten_dict
+        return root_dict
+            
+p1 = Parser()
+f = open('/tmp/ETLMap_any_all.yaml', "r")
+data = dict(yaml.safe_load(f))
+p1.parser()
