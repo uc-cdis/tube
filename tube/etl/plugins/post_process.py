a35543feaa867ba1184474dd057de39ac6357949
Alexander VanTol <avantol13@gmail.com>
2019-07-25 15:25:13 -0500

    fix(arborist): resource paths cant have dashes

--- tube/etl/plugins/post_process.py
+++ tube/etl/plugins/post_process.py
@@ -1,11 +1,17 @@
 def add_auth_resource_path(df):
     # add 'auth_resource_path' to resulting es document if 'project_id' exist
-    if 'project_id' in df[1]:
-        project_id = df[1]['project_id']
+    if "project_id" in df[1]:
+        project_id = df[1]["project_id"]
         if project_id is not None:
-            program_name, project_code = project_id.split('-', 1)
-            df[1]['auth_resource_path'] = '/programs/{}/projects/{}'.format(program_name, project_code)
+            program_name, project_code = project_id.split("-", 1)
+
+            # arborist resources do not allow - but do allow _
+            project_code = str(project_code).replace("-", "_")
+
+            df[1]["auth_resource_path"] = "/programs/{}/projects/{}".format(
+                program_name, project_code
+            )
         else:
-            df[1]['auth_resource_path'] = ''
+            df[1]["auth_resource_path"] = ""
 
     return df[0], df[1]
