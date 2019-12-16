6a8df4b1eba9cf62e6232d2e15259ac279fd93c3
vpsx <19900057+vpsx@users.noreply.github.com>
2019-12-16 15:36:16 -0600

    chore(py3): itertools.izip -> built-in zip

--- tests/utils.py
+++ tests/utils.py
@@ -36,4 +36,4 @@ def pairwise(iterable):
     """s -> (s0,s1), (s1,s2), (s2, s3), ..."""
     a, b = itertools.tee(iterable)
     next(b, None)
-    return itertools.izip(a, b)
+    return zip(a, b)
