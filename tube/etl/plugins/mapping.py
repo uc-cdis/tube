a7d65c96b45d012ca4c634c15d4b5c8a686e8837
Thanh Dang Nguyen <thanhnd.fit@gmail.com>
2022-12-16 07:49:08 -0600

    fix(mapping): fix mapping type of auth_resource_path (#216)
    * fix(mapping): fix mapping type of auth_resource_path
    
    * Test #216 (#217)
    
    * Test fix
    
    * Add instructions for 'mock_config_files'
    
    Co-authored-by: Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>

--- tube/etl/plugins/mapping.py
+++ tube/etl/plugins/mapping.py
@@ -1,8 +1,10 @@
 def add_auth_resource_path_mapping(doc_type, types):
-    properties = types.get(doc_type).get("properties")
-    if "project_id" in properties:
-        types[doc_type]["properties"]["auth_resource_path"] = {
-            "type": "keyword",
-            "fields": {"analyzed": {"type": "text"}},
-        }
+    types[doc_type]["properties"]["auth_resource_path"] = {
+        "type": "keyword",
+        "fields": {"analyzed": {"type": "text"}},
+    }
+    types[doc_type]["properties"]["project_id"] = {
+        "type": "keyword",
+        "fields": {"analyzed": {"type": "text"}},
+    }
     return types
