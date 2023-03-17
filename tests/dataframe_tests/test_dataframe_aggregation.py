4954fb92a9be40ea5768954f17ba2286ffd2295b
Thanh Nguyen <thanhnd.fit@gmail.com>
2023-03-17 02:32:40 -0500

    fix(test): add test case where project_id not exist

--- tests/dataframe_tests/test_dataframe_aggregation.py
+++ tests/dataframe_tests/test_dataframe_aggregation.py
@@ -21,10 +21,10 @@ def test_get_direct_children_with_parent(spark_context):
     result_df = translator.get_direct_children(input_df)
     assert_dataframe_equality(expected_df, result_df, get_node_id_name("participant"))
 
-def test_ensure_project_id_exist(spark_context):
+def test_ensure_project_id_exist_with_project_id_in_input_df(spark_context):
     """
-    This function is to test function ensure_project_id_exist
-    :param spark_context:
+    This function is to test function ensure_project_id_exist with etlMapping has project_id
+    :param spark_context
     :return: It assert that the dataframe out of ensure_project_id_exist has project_id field
     """
     input_df, expected_df = get_input_output_dataframes(
@@ -35,6 +35,21 @@ def test_ensure_project_id_exist(spark_context):
     translator = get_translator(spark_context, config, "participant", "aggregation")
     result_df = translator.ensure_project_id_exist(input_df)
     assert_dataframe_equality(expected_df, result_df, get_node_id_name("participant"))
+
+def test_ensure_project_id_exist_without_project_id_in_input_df(spark_context):
+    """
+    This function is to test function ensure_project_id_exist
+    :param spark_context
+    :return: It assert that the dataframe out of ensure_project_id_exist has project_id field
+    """
+    input_df, expected_df = get_input_output_dataframes(
+        get_spark_session(spark_context),
+        "project__0_Translator.get_direct_children",
+        "project__0_Translator.ensure_project_id_exist"
+    )
+    translator = get_translator(spark_context, config, "project", "aggregation")
+    result_df = translator.ensure_project_id_exist(input_df)
+    assert_dataframe_equality(expected_df, result_df, get_node_id_name("project"))
 
 # def test_aggregate_nested_properties(spark_context):
 #     _, expected_df = get_input_output_dataframes(
