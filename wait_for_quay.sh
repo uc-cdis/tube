931f3d2c5f0fd65ada744980282552d1a8363602
Thanh Nguyen <thanhnd.fit@gmail.com>
2023-09-14 13:01:48 -0500

    correct sorting function of wait_for_quay

--- wait_for_quay.sh
+++ wait_for_quay.sh
@@ -18,7 +18,8 @@ CURRENT_ATTEMPT=1
 until [ "$QUAY_BUILD_STATUS" == "complete" ] || [ $CURRENT_ATTEMPT -gt $ATTEMPTS ]
 do
   RESPONSE=$(curl -s -XGET https://quay.io/api/v1/repository/${REPOSITORY}/build/)
-  QUAY_BUILD_STATUS=`echo ${RESPONSE} | jq -r '.[] | map(select(.tags[] | contains("'${BRANCH}'")) | {started,phase}) | sort_by(.started) | last | .phase'`
+  FORMATED_RES=$(echo ${RESPONSE} | jq -r '.builds[].started |= (strptime("%A, %d %B %Y %H:%M:%S") | strftime("%Y-%m-%d %H:%M:%S"))')
+  QUAY_BUILD_STATUS=`echo ${FORMATED_RES} | jq -r '.[] | map(select(.tags[] | contains("'${BRANCH}'")) | {started,phase}) | sort_by(.started) | last | .phase'`
   echo "Quay build status: ${QUAY_BUILD_STATUS}"
   echo "Attempt number: ${CURRENT_ATTEMPT}"
   CURRENT_ATTEMPT=$((CURRENT_ATTEMPT + 1))
