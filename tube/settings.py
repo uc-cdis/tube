6dd99d15d039b9a78dbc36dfcb1a7c7a22858179
Andrzej Grzelak <andrzej.grzelak@contractors.roche.com>
2023-11-30 15:26:07 +0100

    Fix duplication of SSL setup in JDBC connection string

--- tube/settings.py
+++ tube/settings.py
@@ -26,13 +26,7 @@ ENV_DB_USE_SSL_BOOL = os.getenv("DB_USE_SSL", "false") in ('true', '1', 't')
 DB_USE_SSL = ENV_DB_USE_SSL_BOOL or conf_data.get(
     "db_use_ssl", False
 )  # optional property to db_use_ssl
-JDBC = (
-    "jdbc:postgresql://{}:{}/{}".format(DB_HOST, DB_PORT, DB_DATABASE)
-    if DB_USE_SSL is False
-    else "jdbc:postgresql://{}:{}/{}?sslmode=require".format(
-        DB_HOST, DB_PORT, DB_DATABASE
-    )
-)
+JDBC = "jdbc:postgresql://{}:{}/{}".format(DB_HOST, DB_PORT, DB_DATABASE)
 PYDBC = "postgresql://{}:{}@{}:{}/{}".format(
     DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE
 )
