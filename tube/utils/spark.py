24d6ad4a9210465625ec2ae035101ae5a4500217
George Thomas <george42@uchicago.edu>
2026-01-01 11:48:23 -0800

    Repartition data frame before writing

--- tube/utils/spark.py
+++ tube/utils/spark.py
@@ -61,6 +61,7 @@ def make_sure_hdfs_path_exist(path, sc=None):
 def save_rdd_of_dataframe(df, path, sc):
     fs, opath, sc = get_hdfs_file_handler(sc)
     fs.delete(opath(path), True)
+    df = df.repartition(400)
     df.write.parquet(path)
 
 
